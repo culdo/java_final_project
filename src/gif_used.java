e1b41b7c5d4e5cf4372503d7d0b811b28ccecdea
Chun Hu <cbc106008@stmail.nptu.edu.tw>
2020-09-25 16:26:45 +0800

    Update README.md, add demo.

---
+++ src/gif_used.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class gif_used {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JComboBox comboBox2;
+    private JTextField textField2;
+    private JRadioButton 排程模式RadioButton;
+    private JRadioButton 機器人模式RadioButton;
+    private JRadioButton 未讀先看模式RadioButton;
+    private JButton 啟動Button;
+    private JButton 停止Button;
+}
